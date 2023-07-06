141858bf6c4a24047d5822c74006d7b4677c846d
idnan Hussain <adnanhussain1993@hotmail.co.uk>
2023-07-06 09:41:57 +0100

    updated work

--- src/server.js
+++ src/server.js
@@ -1,30 +1,31 @@
 const express = require("express");
 const app = express();
-const Book = require("./books/model");
-
+// const Book = require("./books/model");
 require("dotenv").config();
 require("./db/connection");
 
+const Book = require("./modelBooks/model")
+
 
 app.use(express.json())
-app.get("/books", (request, response) => {
-    const book = {
-        title: "The Devil Wears Prada",
-        author: "Lauren Weisberger",
-        genre: ["Novel", "Fiction", "Humor"]
-    } 
-    const successResponse = {
-        message: "Response sent successfully",
-        book: book
-    }
-    response.send(successResponse);
-})
+// app.get("/books/getBooks", async (request, response) => {
+//     const books = await book.find{};
+//     const successResponse = {
+//         message: "Response sent Successfully"
+//     }
+// }
+//     const successResponse = {
+//         message: "Response sent successfully",
+//         book: book
+//     }
+//     response.send(successResponse);
+// })
 
 app.post("/books/addbook", async (request, response) => {
     // response.send("Hello from the post route.")
     console.log(request.body)
     const newBook = await Book.create({
-        // id: Math.floor(Math.random()*50), - now we're linking to a DB this will be automatically generated
+        id: Math.floor(Math.random()*50),// - now we're linking to a DB this will be automatically generated
         title: request.body.title,
         author: request.body.author,
         genre: request.body.genre
@@ -41,8 +42,8 @@ app.post("/books/addbook", async (request, response) => {
     // })
 })
 
-app.put("/books", async (request, response) => {
-    response.send("Hello from the update route.")
+// app.put("/books/updatebookauthor", async (request, response) => {
+//     response.send("Hello from the update route.")
     // console.log(request.body)
     // const newBook = {
     //     id: Math.floor(Math.random()*50),
@@ -60,11 +61,11 @@ app.put("/books", async (request, response) => {
     //     author: "J.K. Rowling",
     //     genre: "Fantasy"
     // })
-})
+// })
 
 
-app.delete("/books", async (request, response) => {
-    response.send("Hello from the delete route.")
+//app.delete("/books", async (request, response) => {
+    //response.send("Hello from the delete route.")
     // console.log(request.body)
     // const newBook = {
     //     id: Math.floor(Math.random()*50),
@@ -82,7 +83,7 @@ app.delete("/books", async (request, response) => {
     //     author: "J.K. Rowling",
     //     genre: "Fantasy"
     // })
-})
+//})
 
 app.listen(5001, () => console.log("Server is listening"));
 
